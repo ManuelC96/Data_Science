25e5a70689cea37a241651b791b56082387c34d8
ManuelC96 <manuelchiocchetta@gmail.com>
2023-09-05 20:47:23 +0200

    First Commit

--- main.py
+++ main.py
@@ -6,62 +6,107 @@ import numpy as np
 import requests as req
 
 # °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
-# Contruct an object
-class ComplexNumber:
-    def __init__(self, re, im):
-        self.re = re
-        self.im = im
+# # Contruct an object to test Dunder Methods (double underscore methods)
+# class ComplexNumber:
+#     def __init__(self, re, im):
+#         self.re = re
+#         self.im = im
 
-    def __str__(self):
-        s = f"{self.re} + i{self.im}"
-        return s
+#     def __str__(self):
+#         s = f"{self.re} + i{self.im}"
+#         return s
 
-    def __eq__(self, other):
-        if isinstance(other, ComplexNumber):
-            return self.re == other.re and self.im == other.im
-        else:
-            raise Exception(f"{other} is not an instance of ComplexNumber")
+#     def __eq__(self, other):
+#         if isinstance(other, ComplexNumber):
+#             return self.re == other.re and self.im == other.im
+#         else:
+#             raise Exception(f"{other} is not an instance of ComplexNumber")
 
-    def __setitem__(self, k, v):
-        if k not in ["re", "im"]:
-            raise Exception(f"{k} is not a vil key for ComplexNumber")
-        elif k == "re":
-            self.re = v
-        elif k == "im":
-            self.im = v
+#     def __setitem__(self, k, v):
+#         if k not in ["re", "im"]:
+#             raise Exception(f"{k} is not a vil key for ComplexNumber")
+#         elif k == "re":
+#             self.re = v
+#         elif k == "im":
+#             self.im = v
 
-    def __getitem__(self, k):
-        if k not in ["re", "im"]:
-            raise Exception(f"{k} is not a vil key for ComplexNumber")
-        elif k == "re":
-            return self.re
-        elif k == "im":
-            return self.im
+#     def __getitem__(self, k):
+#         if k not in ["re", "im"]:
+#             raise Exception(f"{k} is not a vil key for ComplexNumber")
+#         elif k == "re":
+#             return self.re
+#         elif k == "im":
+#             return self.im
           
-    def __add__(self, other):
-        if isinstance(other, ComplexNumber):
-            return ComplexNumber(self.re + other.re, self.im + other.im)
-        elif isinstance(other, int):
-            return ComplexNumber(self.re + other, self.im + other)
-        elif isinstance(other, float):
-            return ComplexNumber(self.re + other, self.im + other)
-        else:
-            raise Exception(f" {type(other)} {other} isn't a valid type")
+#     def __add__(self, other):
+#         if isinstance(other, ComplexNumber):
+#             return ComplexNumber(self.re + other.re, self.im + other.im)
+#         elif isinstance(other, int):
+#             return ComplexNumber(self.re + other, self.im + other)
+#         elif isinstance(other, float):
+#             return ComplexNumber(self.re + other, self.im + other)
+#         else:
+#             raise Exception(f" {type(other)} {other} isn't a valid type")
         
-    def __len__(self):
-        from math import sqrt
-        return int(sqrt(self.re**2 + self.im**2))
+#     def __len__(self):
+#         from math import sqrt
+#         return int(sqrt(self.re**2 + self.im**2))
 
-# initialise the object ComplexNumber
-c1 = ComplexNumber(5, 6)
-print(c1)
+# # initialise the object ComplexNumber
+# c1 = ComplexNumber(5, 6)
+# print(c1)
 
-c2 = ComplexNumber(5, 6)
-print(c2)
-# try various methods
-print(c1)
-print(c1 + 1)
-print(c1 + c2)
-print(c1 + "s")
-c1["re"] = 55
-print(len(c1))
+# c2 = ComplexNumber(5, 6)
+# print(c2)
+# # try various methods
+# print(c1)
+# print(c1 + 1)
+# print(c1 + c2)
+# print(c1 + "s")
+# c1["re"] = 55
+# print(len(c1))
+# °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+# Private attribute and name Mangling
+# class Pearson():
+#     def __init__(self, firstName, lastName):
+#         self.firstName = firstName
+#         self.lastName = lastName
+#     def __str__(self) -> str:
+#         return f"{self.firstName} {self.lastName}"
+    
+# class PrivatePearson(Pearson):
+#     def __init__(self, firstName, lastName, privateInfo):
+#         super().__init__(firstName, lastName)
+#         self.__privateInfo = privateInfo
+    
+#     def __str__(self) -> str:
+#         return super().__str__() + f" {self.__privateInfo}"
+
+# p1 = Pearson("Manuel", "Chiocchetta")
+
+# print(p1)
+
+# p1.firstName="Carlo"
+# print(p1)
+
+# p2Private = PrivatePearson("Manuel", "Chiocchetta", "SecretInfo")
+# print(p2Private)
+
+# p2Private.__privateInfo = "NewSecret"
+# print(dir(p2Private))
+
+# p2Private._PrivatePearson__privateInfo = "NewSecret"
+# print(p2Private)
+# °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+# Exceptions
+
+# import inspect
+
+def exception_tree(cls, ind = 0):
+    print("-" * ind, cls)
+    for i in cls.__subclasses__():
+        exception_tree(i, ind + 2)
+        
+
+
+exception_tree(BaseException)
