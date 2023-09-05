4cae34c7e4034e1da7aabbe2bfba5afed764617c
ManuelC96 <manuelchiocchetta@gmail.com>
2023-09-05 15:19:40 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import requests as req
+
+# °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
