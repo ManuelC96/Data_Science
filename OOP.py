ea9fd2903c0337f8b0ff7482bac50dbff2abb1aa
ManuelC96 <manuelchiocchetta@gmail.com>
2023-09-16 11:19:26 +0200

    First commit

--- OOP.py
+++ OOP.py
@@ -5,7 +5,7 @@ class Item:
     # assign global variable to class, this are called --> Class attribute
     payRate = 0.8
     all = []
-    random = 0
+    
 
     def __init__(self, name: str, price: float, quantity=0) -> None:
         # received arguments validators
@@ -17,12 +17,12 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-        Item.all.append(self)
+        Item.all.append(self)#append initialized item to [all] list
 
     def applyDiscount(self):
         self.price = self.price * self.payRate
 
-    @classmethod
+    @classmethod#creater a class method using decorator, this method is proper of the class omly, not of his instance
     def instanciateFromCsv(cls):
         with open("items.csv") as f:
             reader = csv.DictReader(f)
@@ -30,10 +30,10 @@ class Item:
     # def __str__(self) -> str:
     #     return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-    def __repr__(self) -> str:
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
+    def __repr__(self) -> str: # same as __str__ method represent info when instance obj is argument of print func 
+        return f"Item('{self.name}', {self.price}, {self.quantity})"#best practice is to represent the obj same way we create instance of the class, so we can copy from cmd and change argument only to instantiate a different obj
 
-
+# cs
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
 item3 = Item("Cable", 10, 5)
@@ -42,4 +42,4 @@ item5 = Item("Keyboard", 75, 5)
 
 
 for item in Item.all:
-    print(item)
+
