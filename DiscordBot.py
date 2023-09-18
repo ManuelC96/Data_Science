9f976cba74f89f8a5c660c32de7286078374a5bd
ManuelC96 <manuelchiocchetta@gmail.com>
2023-09-18 17:14:27 +0200

    First commit

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,28 +1,24 @@
-import random
+import os
+from dotenv import load_dotenv
 
-def randomDecorator(func):
-    def wrapper():
-        r = random.randint(0, 1)
-        if r == 0:
-            return "Functio n not executed"
+import discord
 
-        else:
-            return func()
-            
-        
-    return wrapper
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
 
-        
-    
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 
+client = MyClient()
+client.run(f"{TOKEN}")
 
-def hello():
-    return "Hello the func has been executed"
 
-hello = randomDecorator(hello)
-print(type(hello))
 
-# for i in range(10):
-#     print(hello())
+
+
