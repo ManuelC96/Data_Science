34af5fc8ebd1863c1d9418fe11ee5d86cd2da54e
Artefici <arteficidesign@gmail.com>
2023-10-16 19:56:35 +0200

    First commit

--- DataScience/neuralNet.py
+++ DataScience/neuralNet.py
@@ -1,1 +1,12 @@
 # Simple neural network MNIST database
+import numpy as np
+import struct as st
+
+imgPath = r'DataScience\data\MNIST\train-images.idx3-ubyte'
+labelsPath = r'DataScience\data\MNIST\train-labels.idx1-ubyte'
+
+with open(imgPath, 'rb') as imageFile:
+    imageFile.seek(0)#specify bit offset
+    magic = st.unpack('>4b',imageFile.read(4))#magic number
+
+print(magic)
