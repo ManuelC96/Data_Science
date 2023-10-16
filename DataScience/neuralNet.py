24d46f2b8a38f187497f6935754cae93c092f85e
Artefici <arteficidesign@gmail.com>
2023-10-16 20:12:58 +0200

    First commit

--- DataScience/neuralNet.py
+++ DataScience/neuralNet.py
@@ -8,5 +8,11 @@ labelsPath = r'DataScience\data\MNIST\train-labels.idx1-ubyte'
 with open(imgPath, 'rb') as imageFile:
     imageFile.seek(0)#specify bit offset
     magic = st.unpack('>4b',imageFile.read(4))#magic number
+    imageFile.seek(4)#specify bit offset
+    imgNumb = st.unpack('>i',imageFile.read(4))#number of imgs
+    imageFile.seek(4)#specify bit offset
+
+
 
 print(magic)
+print(imgNumb)
