5e04e4d827488971821488c13dc579eac8245e66
ManuelC96 <manuelchiocchetta@gmail.com>
2023-10-17 14:09:56 +0200

    First Commit

--- DataScience/neuralNet.py
+++ DataScience/neuralNet.py
@@ -1,39 +1,73 @@
-# Simple neural network MNIST database
+# simple neural network mnist database
 import numpy as np
 import struct as st
 from matplotlib import pyplot as plt
 import time
 
-imgPath = r'DataScience\data\MNIST\train-images.idx3-ubyte'
-labelsPath = r'DataScience\data\MNIST\train-labels.idx1-ubyte'
+imgpath = r'datascience\data\mnist\train-images.idx3-ubyte'
+labelspath = r'datascience\data\mnist\train-labels.idx1-ubyte'
 
-with open(imgPath, 'rb') as imageFile:#open context manager, read file in binary format
-    imageFile.seek(0)#specify bit offset
-    magic = st.unpack('>4b',imageFile.read(4))#magic number
-    imageFile.seek(4)#specify bit offset
-    imgNumb = st.unpack('>i',imageFile.read(4))#number of imgs
-    imageFile.seek(8)#specify bit offset
-    nRows = st.unpack('>i', imageFile.read(4))#number of rows
-    imageFile.seek(12)#specify bit offset
-    nCols = st.unpack('>i', imageFile.read(4))#number of colums
+# extract numbers froms MNIST
+with open(imgpath, 'rb') as imagefile:#open context manager, read file in binary format
+    imagefile.seek(0)#specify bit offset
+    magic = st.unpack('>4b',imagefile.read(4))#magic number
+    imagefile.seek(4)#specify bit offset
+    imgnumb = st.unpack('>i',imagefile.read(4))#number of imgs
+    imagefile.seek(8)#specify bit offset
+    nrows = st.unpack('>i', imagefile.read(4))#number of rows
+    imagefile.seek(12)#specify bit offset
+    ncols = st.unpack('>i', imagefile.read(4))#number of colums
 
-nOfBytes = imgNumb[0]*nRows[0]*nCols[0]*1
+nofbytes = imgnumb[0]*nrows[0]*ncols[0]*1
 
+with open(imgpath, 'rb') as imagefile:#open context manager, read file in binary format
+    imagefile.seek(16)#specify bit offset
+    # imgArray = np.asarray(st.unpack('>' + f'{nofbytes}' + 'B',imagefile.read(nofbytes))).reshape((imgnumb[0],nrows[0], ncols[0]))
 
+    xVar = np.asarray(st.unpack('>' + f'{nofbytes}' + 'B',imagefile.read(nofbytes))).reshape((imgnumb[0],(nrows[0] * ncols[0]))) 
 
-with open(imgPath, 'rb') as imageFile:#open context manager, read file in binary format
-    imageFile.seek(16)#specify bit offset
-    imgArray = np.asarray(st.unpack('>' + f'{nOfBytes}' + 'B',imageFile.read(nOfBytes))).reshape((imgNumb[0],nRows[0], nCols[0]))
+#/////////////////////////////////////////////////////////////////////////////////////////////// 
+#///////////////////////////////////////////////////////////////////////////////////////////////
+
+# extract labels from MNIST 
+with open(labelspath, 'rb') as labelfile:
+    labelfile.seek(0)
+    magic = st.unpack(">4b", labelfile.read(4))
+    print(magic)
+    labelfile.seek(4)
+    labelnumb = st.unpack('>i',labelfile.read(4))
+    print(labelnumb)
     
+totalbytes = labelnumb[0]
+
+with open(labelspath, "rb") as labelfile:
+    labelfile.seek(8) 
+    labels = np.asarray(st.unpack(">" + f"{totalbytes}" + "b", labelfile.read(totalbytes))).reshape((totalbytes))
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////     
+#///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+# def mimageshow(array: list):
+#     for i in range(len(array)):
+#         plt.imshow(array[i])
+#         plt.pause(1)#plt pause image frame
+#         plt.clf()#plt clear image to show next one
+#     return none
 
-def mImageShow(array: list):
-    for i in range(len(array)):
-        plt.imshow(array[i])
-        plt.pause(1)#plt pause image frame
-        plt.clf()#plt clear image to show next one
-    return None
+# mimageshow(imgaArray)
+#///////////////////////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////////////////////
+# Neural network(data x 10 x 10 = y)
+# layer 0 = Data
+# layer 1 = 10 neuron network
+# layer 2 = 10 neuron network
+# layer 3 = Output
 
-mImageShow(imgArray)
+# layer 1 define the weights w*x(data) + b
+
+xVar = xVar.T
+print(np.shape(xVar))
+
